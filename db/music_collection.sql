a32181331ad767363f5c29dfef544a266be6ff8d
DeuterGraves <puckles@gmail.com>
2018-09-26 15:46:39 +0100

    i can haz database

---
+++ db/music_collection.sql
@@ -0,0 +1,17 @@
+-- drop both - album will depend on artist so it gets dropped first.  artist will get created first.
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+--  now to create!
+
+CREATE TABLE artists(
+id SERIAL8 PRIMARY KEY,
+name VARCHAR(255)
+);
+
+CREATE TABLE albums(
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(255),
+  year INT2,
+  artist_id INT8 REFERENCES artists(id)
+);
